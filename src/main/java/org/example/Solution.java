8103b743af4bfdb618c9266e9cebbb4ee7d5d9e8
annt <annt@gft.com>
2022-12-02 11:58:03 +0000

    Done with stream

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,20 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public Solution(){
+
+    }
+
+    public String largestNumber(int[] nums){
+
+       StringBuilder result = new StringBuilder(Arrays.stream(nums)
+               .mapToObj(x -> String.valueOf(x))
+               .sorted((x, y) -> (x + y).compareTo(y + x) * -1)
+               .collect(Collectors.joining()));
+
+        return result.toString();
     }
 }
--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -1,2 +1,51 @@
-package org.example;public class SolutionTest {
+package org.example;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+
+    static Solution solution;
+
+    String result;
+    int[] nums;
+
+    @BeforeAll
+    static void setup(){
+
+        solution = new Solution();
+    }
+
+    @Test
+    public void onlyOneNumber(){
+
+        nums = new  int[] {5};
+
+        result = "5";
+
+        assertEquals(result, solution.largestNumber(nums));
+    }
+
+    @Test
+    public void TwoNumbers(){
+
+        nums = new  int[] {36, 99};
+
+        result = "9936";
+
+        assertEquals(result, solution.largestNumber(nums));
+    }
+
+    @Test
+    public void FiveNumbers(){
+
+        nums = new  int[] {4,5,3,2,55};
+
+        result = "555432";
+
+        assertEquals(result, solution.largestNumber(nums));
+    }
+
 }
